89a2695d85b84983ffd664695e155f2d31426278
Harshit Aditya <harshitaditya27@gmail.com>
2022-07-05 14:19:42 +0530

    Implemented Server Side Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from "../typing"
+interface Props {
+  posts: [Post];
+}
 
-const Home: NextPage = () => {
+export default function Home({ posts }: Props) {
+  console.log(posts);
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -26,4 +31,24 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post" ]{
+    _id, 
+    title, 
+    slug, 
+    author ->{
+  name, 
+    image
+  }, 
+  description, 
+  mainImage, 
+  slug
+  }`;
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
